95377e961101bab50faabb5115420b272e8f5631
dyorgie <tejjlagmay@gmail.com>
2024-11-18 08:03:57 +0800

    added about page

--- finals/src/App.js
+++ finals/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./pages/Login";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
-      <Login></Login>
+      <About></About>
     </>
   );
 }
