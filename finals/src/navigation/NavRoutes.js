0c8bbda64230ab9e29341628ab389518806c82cd
dyorgie <tejjlagmay@gmail.com>
2024-11-22 00:54:41 +0800

    edited files

--- finals/src/navigation/NavRoutes.js
+++ finals/src/navigation/NavRoutes.js
@@ -1,22 +1,34 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/HomePage";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 export default function NavRoutes() {
   const { user } = useContext(UserContext);
 
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={user ? <About /> : <Navigate to="/login" />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+      <Route
+        path="/about"
+        element={user ? <About /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/contact"
+        element={user ? <Contact /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/register"
+        element={user ? <Register /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/login"
+        element={user ? <Login /> : <Navigate to="/login" />}
+      />
+    </Routes>
   );
 }
